c5a0e89535a8d56e9176df0a76d69a67f5fc8ed2
Alessio <avvertix@users.noreply.github.com>
2023-08-20 14:38:40 +0200

    Build (#1)
    * bundle to single file using rollup
    
    * Add build script
    
    * Track current working directory
    
    * Build single executable
    
    * Add GitHub Actions workflow
    
    * Rename binaries before publish

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import meow from 'meow';
 import og from './src/og.js';
+import {cwd} from 'process';
 
 const cli = meow(`
 	Usage
@@ -10,18 +11,29 @@ const cli = meow(`
 	  --output, -o  Output file name
 
 	Examples
-	  $ og file.html --svg
+	  $ og file.html
 `, {
 	importMeta: import.meta,
 	flags: {
 		output: {
 			type: 'string',
 			shortFlag: 'o'
+		},
+		width: {
+			type: 'number',
+			shortFlag: 'w',
+			default: 1200,
+		},
+		height: {
+			type: 'number',
+			shortFlag: 'h',
+			default: 628,
 		}
 	}
 });
 
 og(cli.input.at(0), {
-	output: 'image.png',
+	cwd: cwd(),
+	output: 'card.svg',
 	...cli.flags
 });
--- src/og.js
+++ src/og.js
@@ -1,82 +1,55 @@
 import * as fs from 'fs';
 
+import { join } from 'path';
+
 import satori from 'satori'
 
 import parse from 'html-react-parser'
 
-import { Resvg } from '@resvg/resvg-js'
-
 
 export default async function og(htmlFilePath, flags) {
 
-    console.log("Generating image...");
+  console.log(`Generating SVG from ${htmlFilePath}...`);
 
-    console.log(flags);
+  if (!htmlFilePath) {
+    throw new Error("Specify an html file path");
+  }
 
-    // const htmlFilePath = arguments[0];
-    // const width = arguments[1];
-    // const height = arguments[2];
-    // const fonts = JSON.parse(arguments[3]);
+  const html = fs.readFileSync(htmlFilePath, { encoding: 'utf8', flag: 'r' });
 
-    const html = fs.readFileSync(htmlFilePath, { encoding:'utf8', flag:'r' });
+  // fonts.length
+  //             ? fonts.map((font) => ({
+  //                 name: font.name,
+  //                 data: fs.readFileSync(font.path),
+  //                 weight: font.weight,
+  //                 style: font.style,
+  //             }))
+  //             : 
 
-    // fonts.length
-    //             ? fonts.map((font) => ({
-    //                 name: font.name,
-    //                 data: fs.readFileSync(font.path),
-    //                 weight: font.weight,
-    //                 style: font.style,
-    //             }))
-    //             : 
-
-    const svg = await satori(
-        parse(html),
+  const svg = await satori(
+    parse(html),
+    {
+      width: flags.width || 1200,
+      height: flags.height || 628,
+      fonts: [
         {
-            width: 1200,
-            height: 628,
-            fonts: [
-                    {
-                        name: 'Inter',
-                        data: fs.readFileSync(`./fonts/Inter-Regular.ttf`),
-                        weight: 400,
-                        style: 'regular',
-                    },
-                    {
-                        name: 'Inter',
-                        data: fs.readFileSync(`./fonts/Inter-Bold.ttf`),
-                        weight: 700,
-                        style: 'bold',
-                    },
-                ]
+          name: 'Inter',
+          data: fs.readFileSync(join(flags.cwd, `fonts/Inter-Regular.ttf`)),
+          weight: 400,
+          style: 'regular',
         },
-    );
-
-    const opts = {
-        // background: 'rgba(238, 235, 230, .9)',
-        fitTo: {
-          mode: 'width',
-          value: 1200,
+        {
+          name: 'Inter',
+          data: fs.readFileSync(join(flags.cwd, `fonts/Inter-Bold.ttf`)),
+          weight: 700,
+          style: 'bold',
         },
-        font: {
-          fontFiles: [
-            './fonts/Inter-Regular.ttf',
-            './fonts/Inter-Bold.ttf'
-          ], // Load custom fonts.
-          loadSystemFonts: false, // Faster to disable loading system fonts.
-          defaultFontFamily: 'Inter',
-        },
-        // imageRendering: 1,
-        // shapeRendering: 2,
-        // logLevel: 'debug', // Default Value: error
-      }
+      ]
+    },
+  );
 
-    const resvg = new Resvg(svg, opts)
-    const pngData = resvg.render()
-    const pngBuffer = pngData.asPng()
 
-    // console.log(svg);
-
-    await fs.promises.writeFile(flags.output, pngBuffer);
+  await fs.promises.writeFile(flags.output, svg);
 
 
 }
